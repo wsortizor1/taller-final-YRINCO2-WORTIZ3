06bd7bead5c649e7c32f24b1f5ed5e4852eae885
yrinco2 <ing.yesidrincon@gmail.com>
2024-08-16 16:13:39 -0500

    cambios de yesid

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-
-
-        System.out.println("Hello world!");
+        System.out.println("Cambio de YRINCO2 16/08/2024");
     }
 }
