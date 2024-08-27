fcd5a650b9d50630e5747b8044dacedb88b17084
yrinco2 <ing.yesidrincon@gmail.com>
2024-08-27 16:42:27 -0500

    se actualiza

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Cambio de YRINCO2 16/08/2024");
+        System.out.println( "----------------------------------------------- \n" +
+                "-       MENU CAPITULO DE PROGRAMACION         - \n" +
+                "-            YESID RINCON                     - \n" +
+                "-            WALTHER ORTIZ                    - \n" +
+                "-----------------------------------------------\n" +
+                "-          1. Datos Primitivos                -\n" +
+                "-          2. String                          -\n" +
+                "-          3. Constantes                      -\n" +
+                "-          4. Tipo de Operadores              -\n" +
+                "-          5. Condicional IF, ELSE, IF ELSE   -\n" +
+                "-          6. Condicional Swhits              -\n" +
+                "-          7. Condicional ternaria            -\n" +
+                "-          8. Bucle DO WHILE                  -\n" +
+                "-          9. Bucle WHILE                     -\n" +
+                "-          10. Bucle FOR                      -\n" +
+                "-           0. Salir                          -\n" +
+        "-----------------------------------------------");
+
+        System.out.println("Digite la opcion : ");
+
     }
 }
